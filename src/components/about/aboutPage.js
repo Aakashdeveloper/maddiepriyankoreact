5f212742ca2b0a109e81625c4aa5f60bae1b361e
AAKASH HANDA <AAKASH HANDA>
2017-03-07 21:22:55 +0530

    a

---
+++ src/components/about/aboutPage.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var React = require('react');
+
+var About = React.createClass({
+	render:function(){
+		return (
+			<div className="jumbotron">
+				<h1>About Us</h1>
+				<p>Learning react relay flux native.</p>
+			</div>
+		);
+	}
+});
+
+module.exports = About;
---
+++ src/components/homePage.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var React = require('react');
+
+var Home = React.createClass({
+	render:function(){
+		return (
+			<div className="jumbotron">
+				<h1>Acadgild</h1>
+				<p>Learning react relay flux native.</p>
+			</div>
+		);
+	}
+});
+
+module.exports = Home;
